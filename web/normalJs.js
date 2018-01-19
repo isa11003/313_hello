1c1cec480badc06176506f01088a578def028f3c
unknown <isa11003@byui.edu>
2018-01-19 16:38:42 -0700

    fixed hover link

--- web/normalJs.js
+++ web/normalJs.js
@@ -2,12 +2,12 @@ function startGif() {
 	var slider = document.getElementById("slider");
 	var gif = document.getElementById("gifContainer");
 
-	if (gif.style.display == "none"){
-		gif.style.display = "block";
-		slider.style.display = "none";
-	}
-	else {
+	if (gif.style.display == "block"){
 		gif.style.display = "none";
 		slider.style.display = "block";
+	}
+	else {
+		gif.style.display = "block";
+		slider.style.display = "none";
 	}
 }
