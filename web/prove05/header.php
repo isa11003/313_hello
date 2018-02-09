617a1a6b20db7e5cb1fa98dc47913eab8782aab2
unknown <isa11003@byui.edu>
2018-02-09 12:59:20 -0700

    making login

--- web/prove05/header.php
+++ web/prove05/header.php
@@ -1,5 +1,5 @@
 <?php
-	echo '<div id="header"> Be Uplifted';
+	echo '<div id="header"> <h1>Be Uplifted</h1>';
 	echo '<a href="login.php">Login</a> </div>';
 	
 	//url: postgres://vioiwqpjaamamo:fbbd47b484391188e149fee848c17ac3321350af1a7f9d7f1e4f24c9fe39db92@ec2-54-235-249-33.compute-1.amazonaws.com:5432/da2aqrlp29fisb
