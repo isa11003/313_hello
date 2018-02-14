ad624e5eafc85dfd0018270c7faa2b33d767351f
unknown <isa11003@byui.edu>
2018-02-13 21:07:50 -0700

    didn't talk to database

--- web/prove05/validate.php
+++ web/prove05/validate.php
@@ -1,35 +1,39 @@
-<?php 
-	if (isSet($_POST['username']))
+<?php
+	
+	$dbUrl = getenv('DATABASE_URL');
+	$dbopts = parse_url($dbUrl);
+	$dbHost = $dbopts["host"];
+	$dbPort = $dbopts["port"];
+	$dbUser = $dbopts["user"];
+	$dbPassword = $dbopts["pass"];
+	$dbName = ltrim($dbopts["path"],'/');
+
+	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+	$username = $_POST['username'];
+	$password = $_POST['p	assword']
+	
+	foreach ($db->query("SELECT * FROM public.user WHERE username = '$username'") as $var)
 	{
-		$username = $_POST['username'];
-		$password = $_POST['password']
-		
-		foreach ($db->query("SELECT * FROM public.user WHERE username = '$username'") as $var)
+		if ($var['username'] == $username)
 		{
-			if ($var['username'] == $username)
+			if ($var['password'] == $password)
 			{
-				if ($var['password'] == $password)
-				{
-					session_start();
-			
-					$_SESSION[$user] = $var['id'];
-				}
-					
-					header("Location: index.php"); /* Redirect browser */
-				}
-				else
-				{	
-					header("Location: login.php"); /* Redirect browser */
-				}
+				session_start();
+		
+				$_SESSION[$user] = $var['id'];
+			}
+				
+				header("Location: index.php"); /* Redirect browser */
 			}
 			else
-			{
+			{	
 				header("Location: login.php"); /* Redirect browser */
 			}
-		} 
-	}
-	else
-	{
-		header("Location: login.php"); /* Redirect browser */
-	}
+		}
+		else
+		{
+			header("Location: login.php"); /* Redirect browser */
+		}
+	} 
 ?>
